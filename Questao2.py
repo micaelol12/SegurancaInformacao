b5340111876c7373d1e1c18e82799522017ab7fb
Micael Luan Conti <micael.conti@govbr.com.br>
2025-09-15 23:59:56 -0300

    feat: cifra

--- Questao2.py
+++ Questao2.py
@@ -5,15 +5,34 @@ class CercaFerroviaria():
     def __init__(self,ptrilhas):
         self.trilhas = ptrilhas
 
-    def criaMatriz(self,linhas):
-        return [[self.caractere_vazio for _ in range(self.colunas)] for _ in range(linhas)]
+    def criaMatriz(self,colunas):
+        return [[self.caractere_vazio for _ in range(colunas)] for _ in range(self.trilhas)]
 
     def cifrar(self,texto):
         texto = texto.replace(' ','')
-    
+        l_texto = len(texto)
+        cifra = ""
+        matriz = self.criaMatriz(l_texto)
+        i = 0
+        j = 0
+        a = 1
 
-        while 
-
+        while i < l_texto:
+            matriz[j][i] = texto[i]
+            i+=1
+            j += a
+            
+            if j == self.trilhas -1:
+                a = -1
+            if j == 0:
+                a = 1
+        
+        for i in range(self.trilhas):
+            for j in range(l_texto):
+                if matriz[i][j] != '':
+                    cifra += matriz[i][j]
+                    
+        return cifra
         
     def decifrar(self,cifra):
         pass
@@ -21,7 +40,7 @@ class CercaFerroviaria():
 
 
 
-t = CercaFerroviaria(7)
+t = CercaFerroviaria(3)
 cifrado = t.cifrar("VAMOS INVADIR O SUL AMANHA")
 print(cifrado)
 decifrado = t.decifrar(cifrado)
