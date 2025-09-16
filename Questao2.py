81dd37a70fe3be032c7d02f5d1dfb7085f097642
Micael Luan Conti <mlconti@furb.br>
2025-09-15 21:48:57 -0300

    q2

---
+++ Questao2.py
@@ -0,0 +1,30 @@
+class CercaFerroviaria():
+    trilhas = 0
+    caractere_vazio = ''
+
+    def __init__(self,ptrilhas):
+        self.trilhas = ptrilhas
+
+    def criaMatriz(self,linhas):
+        return [[self.caractere_vazio for _ in range(self.colunas)] for _ in range(linhas)]
+
+    def cifrar(self,texto):
+        texto = texto.replace(' ','')
+    
+
+        while 
+
+        
+    def decifrar(self,cifra):
+        pass
+
+
+
+
+t = CercaFerroviaria(7)
+cifrado = t.cifrar("VAMOS INVADIR O SUL AMANHA")
+print(cifrado)
+decifrado = t.decifrar(cifrado)
+print(decifrado)
+
+    
