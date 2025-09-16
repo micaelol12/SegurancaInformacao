1b7f9f2350e26f57ce4d97f23e93d5c72fbc572d
Micael Luan Conti <micael.conti@govbr.com.br>
2025-09-16 09:18:27 -0300

    feat: corrige 1

--- Questao1.py
+++ Questao1.py
@@ -8,50 +8,46 @@ class TransposicaoColunar():
     def __init__(self,pcolunas):
         self.colunas = pcolunas
 
-    def criaMatriz(self,linhas):
+    def _cria_matriz(self,linhas):
         return [[self.caractere_vazio for _ in range(self.colunas)] for _ in range(linhas)]
+    
 
     def cifrar(self,texto):
         texto = texto.replace(' ','')
         linhas = math.ceil(len(texto) / self.colunas)
+        matriz = self._cria_matriz(linhas)
         cifrado = "" 
 
-        matriz = self.criaMatriz(linhas)
-
+        idx = 0
         for i in range(linhas):
             for j in range(self.colunas):
-                posicao = (self.colunas * i) + j
-
-                if posicao >= len(texto):
-                    break
-
-                matriz[i][j] = texto[posicao]
-
+                if idx < len(texto):
+                    matriz[i][j] = texto[idx]
+                    idx += 1
+                
         for j in range(self.colunas):
             for i in range(linhas):
-                c = matriz[i][j]
-                cifrado += c
+                cifrado += matriz[i][j]
 
-        
         return cifrado
+    
         
     def decifrar(self,cifra):
+        linhas = len(cifra) // self.colunas
+        matriz = self._cria_matriz(linhas)
         decifrado = ""
-        linhas = int(len(cifra)/self.colunas)
-        matriz = self.criaMatriz(linhas)
 
+        idx = 0
         for j in range(self.colunas):
             for i in range(linhas):
-                posicao = (linhas * j) + i
-                matriz[i][j] = cifra[posicao]
-        
-        for i in range(linhas):
-            for j in range(self.colunas):
-                decifrado += matriz[i][j]
+                matriz[i][j] = cifra[idx]
+                idx +=1 
+                
+        for linha in matriz:
+            for char in linha:
+                decifrado += char
 
         return decifrado
-
-
 
 
 t = TransposicaoColunar(7)
