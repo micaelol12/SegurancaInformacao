0387f9983ec3d3d74fccdbc856c71c7a68d9e297
MicaelConti <micael.conti@gmail.com>
2025-10-14 18:20:32 -0300

    expansao de chave

--- TrabalhoFinal/AES.py
+++ TrabalhoFinal/AES.py
@@ -1,20 +1,19 @@
+from utils import AES_SBOX
+
 KEY_SIZE = 128
 BLOCK_SIZE = 128
 
-
-Word = list[bytes]
-Key = list[Word]
+Word = list[int]  # 4 bytes como inteiros
+Key = list[Word]  # 4 palavras por round key
 
 class AES():
     def __init__(self):
         self.key = b'ABCDEFGHIJKLMNOP'
         self.get_original_key()
 
-
-    def get_state_matrix(self) -> list[list[bytes]]:
-        matrix = [[self.key[i + j*4] for j in range(4)]for i in range(4)]
+    def get_state_matrix(self) -> list[list[int]]:
+        matrix = [[self.key[i + j*4] for j in range(4)] for i in range(4)]
         return matrix
-    
 
     def get_original_key(self) -> Key:
         state_matrix = self.get_state_matrix()
@@ -26,35 +25,57 @@ class AES():
 
         return original_key
 
-
-    def expand_keys(self):
+    def expand_keys(self) -> list[Key]:
         original_key = self.get_original_key()
-        key_schedule = []
+        key_schedule: list[Key] = []
 
         key_schedule.append(original_key)
 
-        for i in range(1,11):
-            round_key = self.create_round_key(last_round_key=key_schedule[i-1])
+        for i in range(1, 11):
+            round_key = self.create_round_key(last_round_key=key_schedule[i-1], number=i)
             key_schedule.append(round_key)
-    
 
-    def create_round_key(self,last_round_key:Key) -> Key:
+        return key_schedule
+
+    def create_round_key(self, last_round_key: Key, number: int) -> Key:
         round_key = []
 
-        first_word = self.get_round_key_first_word()
+        first_word = self.get_round_key_first_word(last_round_key, number)
         round_key.append(first_word)
 
-        for i in range(1,4):
-            word = self.get_round_key_other_words(i)
+        for i in range(1, 4):
+            prev_word = round_key[i-1]
+            last_word = last_round_key[i]
+            word = [prev_word[j] ^ last_word[j] for j in range(4)]
             round_key.append(word)
 
         return round_key
 
-    def get_round_key_first_word(self):
-        pass
+    def get_round_key_first_word(self, last_round_key: Key, number: int) -> Word:
+        last_word = last_round_key[-1]
+        first_word = last_round_key[0]
+        rotated_word = self.round_bytes(last_word)
+        replaced_word = self.word_replacement(rotated_word)
+        round_constant = self.get_round_constant(number)
+        xor_word = [replaced_word[i] ^ round_constant[i] for i in range(4)]
+        final_word = [first_word[i] ^ xor_word[i] for i in range(4)]
+        
+        return final_word
 
-    def get_round_key_other_words(self,i):
-        pass
+    def word_replacement(self, word: Word) -> Word:
+        return [AES_SBOX[b] for b in word]
+
+    def xtime(self, x: int) -> int:
+        return ((x << 1) ^ 0x1B) & 0xFF if x & 0x80 else (x << 1)
+
+    def round_bytes(self, bytes: Word) -> Word:
+        return bytes[1:] + bytes[:1]
+
+    def get_round_constant(self, number: int) -> Word:
+        rcon = 1
+        for _ in range(1, number):
+            rcon = self.xtime(rcon)
+        return [rcon, 0x00, 0x00, 0x00]
 
     def divide_by_blocks(self):
         pass
@@ -72,4 +93,4 @@ class AES():
         pass
 
 
-aes  = AES()
+aes = AES()
