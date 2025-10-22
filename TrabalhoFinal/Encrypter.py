3ce4411bf52f36cf9b323afa6c1262a3c64d0d96
Micael Luan Conti <micael.conti@govbr.com.br>
2025-10-22 16:36:23 -0300

    feat: validacao

--- TrabalhoFinal/Encrypter.py
+++ TrabalhoFinal/Encrypter.py
@@ -9,7 +9,7 @@ class Options(Enum):
     DECRYPT = (3, "Descriptografar arquivo", "decrypt_file")
     OPTIONS = (4, "Mostrar Opções", "show_options")
     EXIT = (5, "Sair do programa", "exit_program")
-    
+
     def __init__(self, codigo, descricao, metodo):
         self.codigo = codigo
         self.descricao = descricao
@@ -24,7 +24,7 @@ class Encrypter:
     def start(self):
         print("Bem-vindo ao cifrador AES, selecione uma opção:")
         self.show_options()
-        
+
         while True:
             option = self.__get_option()
             method_name = option.metodo
@@ -37,7 +37,7 @@ class Encrypter:
     def set_key(self):
         s = input("Digite a Chave:")
         try:
-            key = list(map(int, s.split(',')))
+            key = self.__format_key(s)
             self.aes = AES(bytes(key))
 
             print("Chave definida com sucesso!")
@@ -70,10 +70,16 @@ class Encrypter:
             print("Decifrado com sucesso")
         except Exception as e:
             print(e)
-            
+
     def exit_program(self):
         print("Encerrando o programa...")
         sys.exit(0)
+
+    def __format_key(self, key):
+        try:
+            return list(map(int, key.split(',')))
+        except Exception:
+            raise Exception("Ocorreu um erro ao ler a chave")
 
     def __get_arquivos(self):
         arquivo_entrada = input("Digite o caminho do arquivo de entrada: ")
@@ -97,10 +103,9 @@ class Encrypter:
 
     def show_options(self):
         for opt in Options:
-            codigo,nome,_ = opt.value
+            codigo, nome, _ = opt.value
             print(f"{codigo} - {nome}")
 
-    
     def __get_option(self) -> Options:
         i = input("Digite o número da opção: ")
         try:
@@ -115,8 +120,6 @@ class Encrypter:
 
         print("Opção inválida.")
         return self.__get_option()
-       
-
 
     def __check_key(self):
         if self.aes is None:
