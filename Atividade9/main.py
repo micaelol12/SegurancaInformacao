640055a16522ea395b1a7077b5bebd910908d910
micaelconti <micael.conti@gmail.com>
2025-11-20 12:31:50 -0300

    .

--- Atividade9/main.py
+++ Atividade9/main.py
@@ -1,29 +1,53 @@
 import jks
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
 
 JKS_PATH = "Atividade9/repositorio.jks" 
 JKS_PASSWORD = "furb2025"     
+FURB_CERT_ALIAS = "*.furb.br (rnp icpedu ov ssl ca 2019)" 
 
-keystore = jks.KeyStore.load(JKS_PATH, JKS_PASSWORD)
 
-for alias, cert in keystore.certs.items():
-    print("\n==============================")
-    print(f"Alias: {alias}")
-    print("==============================")
+def show_certificates_info():
+    keystore = jks.KeyStore.load(JKS_PATH, JKS_PASSWORD)
 
-    cert_obj = x509.load_der_x509_certificate(cert.cert, default_backend())
+    for alias, cert in keystore.certs.items():
+        print("\n==============================")
+        print(f"Alias: {alias}")
+        print("==============================")
 
-    print("a) Proprietário (Subject):")
-    print("   ", cert_obj.subject)
+        cert_obj = x509.load_der_x509_certificate(cert.cert, default_backend())
 
-    print("b) Emissor (Issuer):")
-    print("   ", cert_obj.issuer)
+        print("a) Proprietário (Subject):")
+        print("   ", cert_obj.subject)
 
-    print("c) Autoassinado?: ", "SIM" if cert_obj.subject == cert_obj.issuer else "NÃO")
+        print("b) Emissor (Issuer):")
+        print("   ", cert_obj.issuer)
 
-    print("d) Período de validade:")
-    print("   Não válido antes de:", cert_obj.not_valid_before)
-    print("   Não valido depois de:", cert_obj.not_valid_after)
+        print("c) Autoassinado?: ", "SIM" if cert_obj.subject == cert_obj.issuer else "NÃO")
 
-print("\nFinalizado.")
+        print("d) Período de validade:")
+        print("   Não válido antes de:", cert_obj.not_valid_before)
+        print("   Não valido depois de:", cert_obj.not_valid_after)
+
+    print("\nFinalizado.")
+
+def cipher_key():
+    keystore = jks.KeyStore.load(JKS_PATH, JKS_PASSWORD)
+    cert_der = keystore.certs[FURB_CERT_ALIAS].cert
+    cert_obj = x509.load_der_x509_certificate(cert_der, default_backend())
+    public_key = cert_obj.public_key()
+    aes_key = b"A" * 16     
+
+    encrypted_key = public_key.encrypt(
+        aes_key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    with open("key.aes", "wb") as f:
+        f.write(encrypted_key)
